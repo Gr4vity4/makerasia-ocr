73f0359919b3a04e8179010552f280e5a2bbde7d
Gr4vity4 <prawared.bw@gmail.com>
2020-12-11 18:13:14 +0700

    update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const axios = require("axios");
 const fs = require("fs");
 
 app
+  .use("/img", express.static(path.join(__dirname, "public")))
   .use(bodyParser.json())
   .use(
     bodyParser.urlencoded({
@@ -19,7 +20,22 @@ app
     })
   )
   .get("/", (req, res) => res.send(`Node.js running on port : ${PORT}`))
-  .get("/dev", function (req, res) {
+  .get("/webhook", function (req, res) {
+    console.log(">>> /webhook");
+    // const replyToken = req.body.events[0].replyToken;
+    // const msg = req.body.events[0].message;
+
+    // console.log(`Message token : ${replyToken}`);
+
+    // console.log("Message data");
+
+    // console.log(typeof msg);
+    // console.log(msg);
+    // console.log(`msg id : ${msg.id}`);
+
+    // const URL = `https://api-data.line.me/v2/bot/message/${msg.id}/content`;
+
+    // const lineToken = "__Line_Token__";
     const googleCredentialKey = "AIzaSyCpz3uFpG1uSfvP9RklFzKViQ6JF6-onvg";
 
     axios
@@ -53,5 +69,64 @@ app
         console.log(res.data.responses[0].fullTextAnnotation);
         console.log(res.data.responses[0].fullTextAnnotation.text);
       });
+
+    // axios
+    //   .get(URL, {
+    //     responseType: "stream",
+    //     headers: {
+    //       Authorization: `Bearer {${lineToken}}`,
+    //     },
+    //   })
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(`typeof response : ${typeof response.data}`);
+
+    //     response.data.pipe(fs.createWriteStream("public/lp.jpg"));
+
+    //     console.log(`>>> google result`);
+
+    //     axios
+    //       .post(
+    //         `https://vision.googleapis.com/v1/images:annotate?key=${googleCredentialKey}`,
+    //         {
+    //           requests: [
+    //             {
+    //               image: {
+    //                 source: {
+    //                   imageUri: "https://corgidude.herokuapp.com/img/lp.jpg",
+    //                 },
+    //               },
+    //               features: [
+    //                 {
+    //                   type: "DOCUMENT_TEXT_DETECTION",
+    //                 },
+    //               ],
+    //               imageContext: {
+    //                 languageHints: ["th"],
+    //               },
+    //             },
+    //           ],
+    //         },
+    //         {
+    //           headers: { "content-type": "application/json" },
+    //         }
+    //       )
+    //       .then((res) => {
+    //         // console.log(res)
+    //         console.log(res.data.responses[0].fullTextAnnotation);
+    //         console.log(res.data.responses[0].fullTextAnnotation.text);
+    //       });
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   });
+
+    // console.log(`Message from chat : ${msg}`)
+
+    res.json({
+      status: 200,
+      message: "Webhook is working!",
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
