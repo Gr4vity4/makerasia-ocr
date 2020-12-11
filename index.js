df9d4990e83e7ad73a72b5306224489b25dc687f
Gr4vity4 <prawared.bw@gmail.com>
2020-12-11 23:53:15 +0700

    update

--- index.js
+++ index.js
@@ -23,25 +23,11 @@ app
   .get("/webhook", function (req, res) {
     console.log(">>> /webhook");
     imgPath = req.query.path;
-    // const replyToken = req.body.events[0].replyToken;
-    // const msg = req.body.events[0].message;
 
-    // console.log(`Message token : ${replyToken}`);
-
-    // console.log("Message data");
-
-    // console.log(typeof msg);
-    // console.log(msg);
-    // console.log(`msg id : ${msg.id}`);
-
-    // const URL = `https://api-data.line.me/v2/bot/message/${msg.id}/content`;
-
-    // const lineToken = "__Line_Token__";
     const googleCredentialKey = "AIzaSyDG5ZjQBdiHDNPQVM7mKqoLXseyq0np9aw";
-
     let result = "text not found";
 
-    axios
+    const ret = await axios
       .post(
         `https://vision.googleapis.com/v1/images:annotate?key=${googleCredentialKey}`,
         {
@@ -75,66 +61,12 @@ app
         // console.log(res.data.responses[0].fullTextAnnotation);
         // console.log(">>> debug 3");
         console.log(res.data.responses[0].fullTextAnnotation.text);
-        result = res.data.responses[0].fullTextAnnotation.text;
-
-        res.json({
-          status: 200,
-          message: result,
-        });
+        return {
+          text: res.data.responses[0].fullTextAnnotation.text
+        };
       });
 
-    // axios
-    //   .get(URL, {
-    //     responseType: "stream",
-    //     headers: {
-    //       Authorization: `Bearer {${lineToken}}`,
-    //     },
-    //   })
-    //   .then(function (response) {
-    //     // handle success
-    //     console.log(`typeof response : ${typeof response.data}`);
+    res.json(ret);
 
-    //     response.data.pipe(fs.createWriteStream("public/lp.jpg"));
-
-    //     console.log(`>>> google result`);
-
-    //     axios
-    //       .post(
-    //         `https://vision.googleapis.com/v1/images:annotate?key=${googleCredentialKey}`,
-    //         {
-    //           requests: [
-    //             {
-    //               image: {
-    //                 source: {
-    //                   imageUri: "https://corgidude.herokuapp.com/img/lp.jpg",
-    //                 },
-    //               },
-    //               features: [
-    //                 {
-    //                   type: "DOCUMENT_TEXT_DETECTION",
-    //                 },
-    //               ],
-    //               imageContext: {
-    //                 languageHints: ["th"],
-    //               },
-    //             },
-    //           ],
-    //         },
-    //         {
-    //           headers: { "content-type": "application/json" },
-    //         }
-    //       )
-    //       .then((res) => {
-    //         // console.log(res)
-    //         console.log(res.data.responses[0].fullTextAnnotation);
-    //         console.log(res.data.responses[0].fullTextAnnotation.text);
-    //       });
-    //   })
-    //   .catch(function (error) {
-    //     // handle error
-    //     console.log(error);
-    //   });
-
-    // console.log(`Message from chat : ${msg}`)
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
